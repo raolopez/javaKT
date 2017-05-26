dae5814f036ac8a07d8926673beed3cff055b9b8
Raul Lopez Ordaz <raolopez@raomac.local>
2017-05-26 00:32:36 -0500

    Se agregaron las variables y metodos de instancia a la clase auto.

--- p1/Auto.java
+++ p1/Auto.java
@@ -1,5 +1,17 @@
 
 public class Auto {
+	private String nombre;
+	private String color;
+	private int potencia;
+
+	public void acelerar() {
+		System.out.println("Acelerando...");
+	}
+
+	public void frenar() {
+		System.out.println("Frenando...");
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Hello everyone...");
 	}
